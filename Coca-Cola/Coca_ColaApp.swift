a3b3e324a01703a1e02f05615d0129e84f350a09
goDlin <godlin086@gmail.com>
2020-12-23 17:24:39 +0300

    Initial Commit

---
+++ Coca-Cola/Coca_ColaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coca_ColaApp.swift
+//  Coca-Cola
+//
+//  Created by Денис on 23.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Coca_ColaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
